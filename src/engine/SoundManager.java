ca6a4152d2b35f641aa09f7926ff5ad90f59614b
yunsseong <aster20@hanyang.ac.kr>
2023-10-15 22:00:26 +0900

    feat: update file

--- src/engine/SoundManager.java
+++ src/engine/SoundManager.java
@@ -1,46 +1,47 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package engine;
 
+import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
-import java.io.File;
+import javax.sound.sampled.FloatControl.Type;
 
 public class SoundManager {
     private Clip clip;
 
-    public SoundManager(String filePath){
-        try{
+    public SoundManager(String filePath) {
+        try {
             File file = new File(filePath);
-            if(file.exists()){
-                AudioInputStream sound = AudioSystem.getAudioInputStream(file);
-                clip = AudioSystem.getClip();
-                clip.open(sound);
-            }
-            else{
+            if (!file.exists()) {
                 throw new RuntimeException("sound file not found");
             }
-        }catch(Exception e){
-            System.out.println(e);
+
+            AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+            this.clip = AudioSystem.getClip();
+            this.clip.open(sound);
+        } catch (Exception var4) {
+            System.out.println(var4);
         }
+
     }
 
-    /**  play sound once */
-    public void play(){
-        clip.start();
-    }
-    public void loop(){
-        clip.loop(Clip.LOOP_CONTINUOUSLY);
-    }
-    public void stop(){
-        clip.stop();
-        clip.close();
+    public void play() {
+        this.clip.start();
     }
 
-    public void setVolume(float volume){
-        float minimum = -40, maximum = 6;
-        float value = Math.min((minimum + (0.46f*volume)), maximum);
-        FloatControl floatControl = (FloatControl)(clip.getControl(FloatControl.Type.MASTER_GAIN));
-        floatControl.setValue(value);
+    public void loop() {
+        this.clip.loop(-1);
     }
+
+    public void stop() {
+        this.clip.stop();
+        this.clip.close();
+    }
+
 }
